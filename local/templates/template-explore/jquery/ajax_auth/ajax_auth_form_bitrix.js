d3a10bdff2789bad948f2aac5753819619427077
Sergei <s.albov@maximaster.ru>
2018-04-12 19:07:06 +0300

    #5 Внедрил компонент, но он не работает

---
+++ local/templates/template-explore/jquery/ajax_auth/ajax_auth_form_bitrix.js
@@ -0,0 +1,29 @@
+$(document).ready(() => {/*
+
+console.log(111);
+
+const $button = $('#SUsub');
+const $login = $('#SUemail').val();
+const $pass = $('#SUpass').val();
+const $path = $('#path-path').val();
+
+$button.on('click', ()=>{
+    $.post(
+        $path,
+        {
+            USER_LOGIN: $login,
+            USER_PASSWORD: $pass
+        },
+        onAjaxSuccess
+    );
+
+});
+
+function onAjaxSuccess(data)
+    {
+        console.log(222);
+        alert(data);
+    }*/
+
+});
+
