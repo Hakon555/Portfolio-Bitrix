4c19fe848356a6a7a0c0c1f77a3199c7346426e5
Sergei <s.albov@maximaster.ru>
2018-04-12 15:32:20 +0300

    #4 Изменил шаблон меню, сделал 3 разных меню.

--- local/templates/template-explore/components/bitrix/menu/new_bottom_menu/template.php
+++ local/templates/template-explore/components/bitrix/menu/new_bottom_menu/template.php
@@ -1,36 +1,9 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
-
-<?
-/*echo $arResult[0]["PARAMS"]["NAME_SEC"];
-echo "<pre>";
-//var_dump($arResult);
-print_r($arResult);
-echo "</pre>";*/
-?>
-
 <?if (!empty($arResult)):?>
 
 <?foreach($arResult as $arItem):?>
-
-	<?if($arItem["PARAMS"]["NAME_SEC"]):?>
-	
-		<?if($arItem["PARAMS"]["BLOCK_CLASS"] != "block-2"):?>
-			</div>
-		<?endif?>
-
-		<div class="<?=$arItem["PARAMS"]["BLOCK_CLASS"]?>">
-			<h3><?=$arItem["PARAMS"]["NAME_SEC"]?></h3>
-			<a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
-		
-	<?else:?>
-	
-			<a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
-		
-	<?endif?>
-	
+    <a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
 <?endforeach?>
-
-	</div>
 
 <?endif?>
